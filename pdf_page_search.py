4184887fcc99e38e3b84567286b06b9b04422075
indi-c <indigoconnelly@pm.me>
2024-01-28 02:02:00 +0000

    fixed typos

--- pdf_page_search.py
+++ pdf_page_search.py
@@ -2,18 +2,18 @@ import argparse
 import PyPDF2
 import re
 
-parser = argparse.ArgumentParser(prog="pdf_page_search", description="Outputs a file listing occurences of a string in a pdf")
+parser = argparse.ArgumentParser(prog="pdf_page_search", description="Outputs a file listing occurrences of a string in a pdf")
 
-parser.add_argument("filename", "-f", "--filename", type=str, help="path to the file")
+parser.add_argument("-f", "--filename", metavar="filename", type=str, help="path to the file\n")
 
-parser.add_argument("search_string", "-s", --"string", type=str, help="string to search for")
+parser.add_argument("-s", "--string", metavar="string", type=str, help="string to search for\n")
 
-parser.add_argument("output", "-o", "--output", type=str, help="filepath for output", default="output.txt")
+parser.add_argument("-o", "--output", metavar="output", type=str, help="filepath for output\n", default="output.txt")
 
 args = parser.parse_args()
 
-# if (not(args.filename and args.search_string and args.output)):
-# may not need validation for this if parsed correctly
+if (not(args.filename and args.string and args.output)):
+    raise ValueError("invalid args, please run the program with -h for help")
 
 reader = PyPDF2.PdfReader(args.filename)
 
@@ -21,8 +21,8 @@ file_len = len(reader.pages)
 pages_occurrences = {}
 
 for i in range(file_len):
-    pages_occurrences.get(i, len([s.start() for s in re.finditer(args.search_string, reader.pages[i].extract_text())]))
+    pages_occurrences.get(i, len([s.start() for s in re.finditer(args.string, reader.pages[i].extract_text())]))
 
-with open(args.output) as output:
+with open(args.output, "w+") as output:
     for page, occurrences in pages_occurrences:
-        output.write(f"Page: {page}, Occurrences: {occurrences}")
+        output.write(f"Page: {page}, Occurrences: {occurrences}\n")
